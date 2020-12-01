78477b865ad11fd93a17f01d7c5cd66bf6a87790
Sergey Kiselev <fireados@outlook.com>
2020-12-01 22:50:20 +0300

    Добавьте файлы проекта.

---
+++ Comment.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignProject
+{
+    public class Comment
+    {
+        public int CommentID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Body
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Owner
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int PhotoID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Subject
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int User
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
